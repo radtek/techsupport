cfadc12b41dce01946fe333e4fa529d56639d727
firefoxmmx2 <firefoxmmx@gmail.com>
2012-07-23 17:23:16 +0800

    完善技术部门审批和产品部门审批

--- src/com/aisino2/techsupport/service/impl/ProductDeptApprovalServiceImpl.java
+++ src/com/aisino2/techsupport/service/impl/ProductDeptApprovalServiceImpl.java
@@ -1,17 +1,61 @@
 package com.aisino2.techsupport.service.impl;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import com.aisino2.techsupport.common.Constants;
 import com.aisino2.techsupport.domain.SupportTicket;
 import com.aisino2.techsupport.domain.Tracking;
-import com.aisino2.techsupport.service.IProductDeptApprovalService;
+import com.aisino2.techsupport.service.IDeptApprovalService;
+import com.aisino2.techsupport.service.SupportTicketService;
+import com.aisino2.techsupport.service.TrackingService;
+import com.aisino2.techsupport.workflow.WorkflowUtil;
 
 public class ProductDeptApprovalServiceImpl implements
-		IProductDeptApprovalService {
+		IDeptApprovalService {
+	private SupportTicketService stService;
+	private TrackingService trackingService;
+	
+	/**
+	 * 流程控制服务
+	 */
+	private WorkflowUtil workflow;
+	
+	@Resource(name="SupportTicketServiceImpl")
+	public void setStService(SupportTicketService stService) {
+		this.stService = stService;
+	}
 
+	@Resource(name="TrackingServiceImpl")
+	public void setTrackingService(TrackingService trackingService) {
+		this.trackingService = trackingService;
+	}
+
+	@Resource(name="WorkflowUtil")
+	public void setWorkflow(WorkflowUtil workflow) {
+		this.workflow = workflow;
+	}
+	
 	@Override
-	public void insertProductDeptApproval(String taskId, SupportTicket st,
+	public void insertDeptApproval(String taskId, SupportTicket st,
 			Tracking tracking) {
-		// TODO Auto-generated method stub
-
+		if(Constants.ST_APPR_TYPE_APPR_NOPASS.equals(tracking.getApprovalCode()) ){
+			st.setStStatus(Constants.ST_STATUS_DEPARTMENT_APPRAVAL_NOPASS);
+		}
+		//更新最后操作时间
+		st.setLastUpdateDate(new Date());
+//				保存技术支持单信息
+		stService.updateSupportTicket(st);
+//				保存部门审核意见信息
+		trackingService.insertTracking(tracking);
+		
+		Map<String, Object> var = new HashMap<String, Object>();
+		var.put("productDeprtApprovalResult", Constants.ST_APPR_TYPE_APPR_PASSED
+				.equals(tracking.getApprovalCode()) ? true : false);
+		workflow.getTaskService().completeTask(taskId, var);
 	}
 
 }
--- src/com/aisino2/techsupport/service/impl/TechDeptApprovalServiceImpl.java
+++ src/com/aisino2/techsupport/service/impl/TechDeptApprovalServiceImpl.java
@@ -1,15 +1,60 @@
 package com.aisino2.techsupport.service.impl;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import com.aisino2.techsupport.common.Constants;
 import com.aisino2.techsupport.domain.SupportTicket;
 import com.aisino2.techsupport.domain.Tracking;
-import com.aisino2.techsupport.service.ITechDeptApprovalService;
+import com.aisino2.techsupport.service.IDeptApprovalService;
+import com.aisino2.techsupport.service.SupportTicketService;
+import com.aisino2.techsupport.service.TrackingService;
+import com.aisino2.techsupport.workflow.WorkflowUtil;
 
-public class TechDeptApprovalServiceImpl implements ITechDeptApprovalService {
+public class TechDeptApprovalServiceImpl implements IDeptApprovalService {
+	private SupportTicketService stService;
+	private TrackingService trackingService;
+
+	/**
+	 * 流程控制服务
+	 */
+	private WorkflowUtil workflow;
+
+	@Resource(name = "SupportTicketServiceImpl")
+	public void setStService(SupportTicketService stService) {
+		this.stService = stService;
+	}
+
+	@Resource(name = "TrackingServiceImpl")
+	public void setTrackingService(TrackingService trackingService) {
+		this.trackingService = trackingService;
+	}
+
+	@Resource(name = "WorkflowUtil")
+	public void setWorkflow(WorkflowUtil workflow) {
+		this.workflow = workflow;
+	}
 
 	@Override
-	public void insertTechDeptApproval(String taskId, SupportTicket st,
+	public void insertDeptApproval(String taskId, SupportTicket st,
 			Tracking tracking) {
-		
+		if (Constants.ST_APPR_TYPE_APPR_NOPASS.equals(tracking
+				.getApprovalCode())) {
+			st.setStStatus(Constants.ST_STATUS_DEPARTMENT_APPRAVAL_NOPASS);
+		}
+		// 更新最后操作时间
+		st.setLastUpdateDate(new Date());
+		// 保存技术支持单信息
+		stService.updateSupportTicket(st);
+		// 保存部门审核意见信息
+		trackingService.insertTracking(tracking);
+		Map<String, Object> var = new HashMap<String, Object>();
+		var.put("techDeprtApprovalResult", Constants.ST_APPR_TYPE_APPR_PASSED
+				.equals(tracking.getApprovalCode()) ? true : false);
+		workflow.getTaskService().completeTask(taskId, var);
 	}
 
 }
