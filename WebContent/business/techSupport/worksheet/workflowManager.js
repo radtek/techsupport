17048ae8d1c54adf2dcfbee0dacc825baccce21b
firefoxmmx2 <firefoxmmx@gmail.com>
2012-07-24 14:23:03 +0800

    添加部署XML流程 删除已发布的 流程等函数

--- WebContent/business/techSupport/worksheet/workflowManager.js
+++ WebContent/business/techSupport/worksheet/workflowManager.js
@@ -9,13 +9,19 @@
  * @param data
  */
 function success_callback(data){
+	if(!data.returnNo){
+		$('#iframe').load('business/techSupport/worksheet/workflowManager.jsp');
+	}
+	else{
+		alert(data.returnMsg);
+	}
 	
 }
 /**
  * 发布基于xml的新流程
  */
 function deployByXml(){
-	var url = "";
+	var url = BUSNEISS_PATH +"/deploy_by_xml_worksheet.action";
 	$.post(url,null,success_callback,'json');
 }
 
@@ -31,9 +37,9 @@ function deployOnline(){
  * 的时候，他会清除所有还在使用这个流程的实例，不过也他会
  * 清除业务数据，只清除流程数据。
  */
-function removeDeployment(taskId){
-	var url = "";
-	var para = {taskId:taskId};
+function removeDeployment(deploy_id){
+	var url = BUSNEISS_PATH +"/remove_deployment_worksheet.action";
+	var para = {deploy_id:deploy_id};
 	$.post(url,para,success_callback,'json');
 }
 
