18675ceeb2d30c59ae7a3b48611e28f28cf6c5f2
firefoxmmx2 <firefoxmmx@gmail.com>
2012-07-27 17:42:28 +0800

    指派负责人用户ID必须为字符串

--- src/com/aisino2/techsupport/workflow/assignee/ReassignStleaderAssignmentHandler.java
+++ src/com/aisino2/techsupport/workflow/assignee/ReassignStleaderAssignmentHandler.java
@@ -1,5 +1,7 @@
 package com.aisino2.techsupport.workflow.assignee;
 
+import java.util.Map;
+
 import javax.servlet.ServletContext;
 
 import org.jbpm.api.model.OpenExecution;
@@ -24,18 +26,23 @@ public class ReassignStleaderAssignmentHandler implements AssignmentHandler {
 	 */
 	private static final long serialVersionUID = 9156961200654075794L;
 
-	private ApplicationContext context;
-	private SupportTicketService st_service;
-	private IDepartmentService department_service;
-	private IUserService user_service;
-	private ServletContext sc;
+//	private ApplicationContext context;
+//	private SupportTicketService st_service;
+//	private IDepartmentService department_service;
+//	private IUserService user_service;
+//	private ServletContext sc;
 	@Override
 	public void assign(Assignable assignable, OpenExecution exec) throws Exception {
-		context = WebApplicationContextUtils.getWebApplicationContext(sc);
-		st_service = (SupportTicketService) context.getBean("SupportTicketServiceImpl");
-		department_service = (IDepartmentService) context.getBean("departmentService");
-		user_service = (IUserService) context.getBean("userService");
-		
+//		context = WebApplicationContextUtils.getWebApplicationContext(sc);
+//		st_service = (SupportTicketService) context.getBean("SupportTicketServiceImpl");
+//		department_service = (IDepartmentService) context.getBean("departmentService");
+//		user_service = (IUserService) context.getBean("userService");
+		Map<String, Object> var = (Map<String, Object>) exec.getVariables();
+		for(String key : var.keySet()){
+			if(key.contains("departApprUserId__")){
+				assignable.addCandidateUser(String.valueOf(var.get(key)));
+			}
+		}
 	}
 
 }
