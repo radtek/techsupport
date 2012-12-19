cf8bc363865ed2fbe7367a69b37ad5b836e61647
firefoxmmx2 <firefoxmmx@gmail.com>
2012-12-19 17:33:39 +0800

    添加切面方式的自动环节提示的单元测试. 添加后台方式发送邮件的方法的单元测试.

---
+++ src/com/aisino2/techsupport/test/AopTest.java
@@ -0,0 +1,64 @@
+package com.aisino2.techsupport.test;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.aisino2.techsupport.domain.SupportTicket;
+import com.aisino2.techsupport.domain.Tracking;
+
+public class AopTest {
+	private ApplicationContext context;
+	
+	@Before
+	public void init(){
+		context = new ClassPathXmlApplicationContext(new String[] {
+				"/config/spring/applicationContext.xml",
+				"/config/spring/techsupport-common.xml",
+				"/config/spring/sysadmin-*.xml"});
+	}
+	
+	@Test
+	public void testAop(){
+		BindArg arg = (BindArg) context.getBean("bindArg");
+		SupportTicket st = new SupportTicket();
+		arg.invoke(st);
+		BindArg2 arg2 = (BindArg2) context.getBean("bindArg2");
+		arg2.invoke("1", st);
+		BindArg3 arg3 = (BindArg3) context.getBean("bindArg3");
+		arg3.invoke("2", st, null);
+	}
+}
+
+class BindArg{
+	public void invoke(SupportTicket st){
+		st.setId(1);
+	}
+}
+
+class BindArg2{
+	public void invoke(String taskId,SupportTicket st){
+		st.setId(2);
+	}
+}
+
+class BindArg3{
+	public void invoke(String taskId,SupportTicket st,Tracking track){
+		st.setId(3);
+	}
+}
+
+class BindTest{
+	public void testBind(SupportTicket st){
+		System.out.println("testBind 的 st.getId() == "+st.getId());
+	}
+	public void testBind2(String taskId,SupportTicket st){
+		System.out.println("testBind 的 st.getId() == "+st.getId());
+		System.out.println("testBind 的 taskId == "+taskId);
+	}
+	public void testBind3(String taskId,SupportTicket st,Tracking track){
+		System.out.println("testBind 的 st.getId() == "+st.getId());
+		System.out.println("testBind 的 taskId == "+taskId);
+	}
+}
---
+++ src/com/aisino2/techsupport/test/MailTest.java
@@ -0,0 +1,60 @@
+package com.aisino2.techsupport.test;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.aisino2.sysadmin.service.IDict_itemService;
+import com.aisino2.techsupport.common.CommonUtil;
+import com.aisino2.techsupport.domain.Mail;
+import com.aisino2.techsupport.service.MailService;
+
+public class MailTest {
+	private MailService mailService;
+	private ApplicationContext context;
+	private IDict_itemService dict_itemService;
+	private CommonUtil util;
+	
+	@Before
+	public void init(){
+		context = new ClassPathXmlApplicationContext(new String[] {
+				"/config/spring/applicationContext.xml",
+				"/config/spring/techsupport-common.xml",
+				"/config/spring/sysadmin-*.xml" });
+		mailService = (MailService) context.getBean("mailServiceImpl");
+		util = (CommonUtil)context.getBean("CommonUtil");
+		dict_itemService = (IDict_itemService) context.getBean(
+				"dict_itemService");
+	}
+	@Test
+	public void testMailSendByDaemon() throws Exception {
+		Mail mail = new Mail();
+		// 读取邮件内容信息
+		InputStream in = this.getClass().getClassLoader()
+				.getResourceAsStream("mailContent.properties");
+		Properties properties = new Properties();
+		properties.load(in);
+		// 读取邮件配置信息
+		Properties mailConfig = new Properties();
+		InputStream config = this.getClass().getClassLoader()
+				.getResourceAsStream("mailConfig.properties");
+		mailConfig.load(config);
+		mail.setUser(properties.getProperty("company_send_name"));
+		mail.setEmail(properties.getProperty("company_Address"));
+		mail.setPassword(properties.getProperty("compnay_password"));
+		// mail.setEmail(email);
+		mail.setProtocol(mailConfig.getProperty("protocol"));
+		mail.setSmtphost(mailConfig.getProperty("smtphost"));
+		mail.setHost(mailConfig.getProperty("host"));
+		mailService.connect(mail, true, true, false);
+		String mailContent = properties.getProperty("automessage.tracking_content");
+		mailContent = util.getMsg(mailContent, new String[]{"ffmmx","2012-12-21"});
+		mailService.sendByDaemon(mail, properties.getProperty("automessage.tracking_subject"), "firefoxmmx@gmail.com", null, mailContent, false);
+		Thread.sleep(8000);
+	}
+	
+}
