8a65ea5464ef283f74484a54e39061f6e17804db
firefoxmmx2 <firefoxmmx@gmail.com>
2013-03-19 14:16:00 +0800

    有了一个新的过滤部门审批人的方法来代替以前过期的

--- src/com/aisino2/techsupport/service/impl/ApplyServiceImpl.java
+++ src/com/aisino2/techsupport/service/impl/ApplyServiceImpl.java
@@ -1,5 +1,6 @@
 package com.aisino2.techsupport.service.impl;
 
+import java.io.IOException;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -11,6 +12,7 @@ import org.jbpm.api.ProcessInstance;
 import org.springframework.stereotype.Component;
 
 import com.aisino2.core.service.BaseService;
+import com.aisino2.sysadmin.domain.Department;
 import com.aisino2.sysadmin.domain.Dict_item;
 import com.aisino2.sysadmin.domain.Role;
 import com.aisino2.sysadmin.domain.User_role;
@@ -23,192 +25,254 @@ import com.aisino2.techsupport.domain.SupportTicket;
 import com.aisino2.techsupport.service.ApplyService;
 import com.aisino2.techsupport.service.SupportTicketService;
 import com.aisino2.techsupport.workflow.WorkflowUtil;
+
 @Component("ApplyServiceImpl")
 public class ApplyServiceImpl extends BaseService implements ApplyService {
 	private SupportTicketService stService;
 	private IDict_itemService dictitemService;
 	private IUser_roleService userRoleService;
 	private IRoleService roleService;
-	private CommonUtil util;
-	
 	/**
 	 * 流程控制服务
 	 */
 	private WorkflowUtil workflow;
-	
-	public String insertApplyAndGo(SupportTicket st) {
+	private CommonUtil util;
+
+	public String insertApplyAndGo(SupportTicket st){
 		SupportTicket supportTicket = null;
-		//设置支持单流程状态 为 待公司审批
-//		st.setStStatus(Constants.ST_STATUS_GOING);
+		// 设置支持单流程状态 为 待公司审批
+		// st.setStStatus(Constants.ST_STATUS_GOING);
 		st.setStStatus(Constants.ST_STATUS_WAIT_COMPANY_APPRAVAL);
-		//更新最后操作时间
+		// 更新最后操作时间
 		st.setLastUpdateDate(new Date());
-		//填报时间
+		// 填报时间
 		st.setApplyDate(st.getLastUpdateDate());
-		try{
-			
-			supportTicket=this.stService.insertSupportTicket(st);
+		try {
+
+			supportTicket = this.stService.insertSupportTicket(st);
 			// 指派下一个环节操作用户 --
-			// 开始流程 
+			// 开始流程
 			Map<String, Object> params = new HashMap<String, Object>();
 			params.put("worksheetno", supportTicket.getId());
-			
+
 			Map<String, Object> candidateUsers = new HashMap<String, Object>();
-//			//指派追踪批复用户
-//			if(supportTicket.getSupportLeader()!=null)
-//				candidateUsers.put("trackingUsers", "");
-//			公司审核
-			Role cerole=new Role();
+			// //指派追踪批复用户
+			// if(supportTicket.getSupportLeader()!=null)
+			// candidateUsers.put("trackingUsers", "");
+			// 公司审核
+			Role cerole = new Role();
 			cerole.setRolename(Constants.ST_ROLE_NAME_CE);
-			try{
-				cerole=(Role) roleService.getListRole(cerole).get(0);
-			}catch (IndexOutOfBoundsException e) {
-				
-				RuntimeException a= new RuntimeException("角色["+Constants.ST_ROLE_NAME_CE+"] 未创建..");
-				log.error(a,a.fillInStackTrace());
+			try {
+				cerole = (Role) roleService.getListRole(cerole).get(0);
+			} catch (IndexOutOfBoundsException e) {
+
+				RuntimeException a = new RuntimeException("角色["
+						+ Constants.ST_ROLE_NAME_CE + "] 未创建..");
+				log.error(a, a.fillInStackTrace());
 				throw a;
 			}
-			User_role ceUser_role=new User_role();
+			User_role ceUser_role = new User_role();
 			ceUser_role.setRoleid(cerole.getRoleid());
-			List<User_role> ceUserRoleList = userRoleService.getListUser_role(ceUser_role);
-			String ceApprovalUsersID="";
-			if(ceUserRoleList!=null && ceUserRoleList.size() > 0){
+			List<User_role> ceUserRoleList = userRoleService
+					.getListUser_role(ceUser_role);
+			String ceApprovalUsersID = "";
+			if (ceUserRoleList != null && ceUserRoleList.size() > 0) {
 				StringBuffer ceApprovalUsersIDBuffer = new StringBuffer();
-				for(User_role userRole1 : ceUserRoleList){
+				for (User_role userRole1 : ceUserRoleList) {
 					ceApprovalUsersIDBuffer.append(userRole1.getUserid());
 					ceApprovalUsersIDBuffer.append(",");
 				}
-				ceApprovalUsersID=ceApprovalUsersIDBuffer.substring(0, ceApprovalUsersIDBuffer.length()-1);
+				ceApprovalUsersID = ceApprovalUsersIDBuffer.substring(0,
+						ceApprovalUsersIDBuffer.length() - 1);
 			}
 			candidateUsers.put("ceApprovalUsers", ceApprovalUsersID);
-			
-//			指派负责单位
-			if(supportTicket.getSupportDeptList()!=null && supportTicket.getSupportDeptList().size()>0)
-				candidateUsers.putAll(workflow.setDeptAssigneeVariable(supportTicket.getSupportDeptList()));
-//			指派反馈确认用户
-			//具有反馈人角色的，并且关联了区总角色的
-//			字典映射的各区总角色
+
+			// 指派负责单位
+			if (supportTicket.getSupportDeptList() != null
+					&& supportTicket.getSupportDeptList().size() > 0) {
+				// 使用新版的指派机构
+				String departmentApprovalUsersId = "";
+				for (Department supportDepartment : supportTicket
+						.getSupportDeptList()) {
+					User_role userRole = new User_role();
+
+					try {
+						if (util.getTechSupportEnvConfig(
+								"techsupport.worksheet.approval_department.tech")
+								.equals(supportDepartment.getDepartcode())) {
+							// 指派技术开发部
+							Role role = new Role();
+							role.setRolename(Constants.ST_ROLE_TECH_DEPT_APPR_TYPE);
+							role = (Role) roleService.getListRole(role).get(0);
+							userRole.setRoleid(role.getRoleid());
+
+						} else if (util
+								.getTechSupportEnvConfig(
+										"techsupport.worksheet.approval_department.product")
+								.equals(supportDepartment.getDepartcode())) {
+							// 指派产品方案部
+							Role role = new Role();
+							role.setRolename(Constants.ST_ROLE_PRODUCT_DEPT_APPR_TYPE);
+							role = (Role) roleService.getListRole(role).get(0);
+							userRole.setRoleid(role.getRoleid());
+						}
+					} catch (IOException e) {
+						log.error(e);
+						throw new RuntimeException(e);
+					}
+					if (userRole.getRoleid() == null
+							|| userRole.getRoleid() == 0)
+						throw new RuntimeException(
+								"指派负责部门发生错误，无法获取到负责部门对应的角色信息");
+					List<User_role> lAssigneeUsers = userRoleService
+							.getListUser_role(userRole);
+					for (User_role ur : lAssigneeUsers) {
+						departmentApprovalUsersId += ur.getUserid() + ",";
+					}
+				}
+				departmentApprovalUsersId = departmentApprovalUsersId.length() > 0 ? departmentApprovalUsersId
+						.substring(0, departmentApprovalUsersId.length() - 1)
+						: "";
+				candidateUsers.put("deptApprovalUsers",
+						departmentApprovalUsersId);
+			}
+
+			// 指派反馈确认用户
+			// 具有反馈人角色的，并且关联了区总角色的
+			// 字典映射的各区总角色
 			Dict_item dictitem = new Dict_item();
 			dictitem.setDict_code(Constants.ST_RGM_RG_MAP_DICT_CODE);
 			dictitem.setFact_value(st.getRegion());
-			dictitem = dictitemService.getDictionaryItemByDictCodeAndFactValue(dictitem);
-			
-			if(dictitem==null)
+			dictitem = dictitemService
+					.getDictionaryItemByDictCodeAndFactValue(dictitem);
+
+			if (dictitem == null)
 				throw new RuntimeException("该区域没有指定区总");
-			String feedbackUsersStr=""	;
-			Role rgmRole=new Role();
+			String feedbackUsersStr = "";
+			Role rgmRole = new Role();
 			Role feedback_role = new Role();
 			rgmRole.setRolename(dictitem.getDisplay_name());
-			List<Role> rgmRoleList=roleService.getListRole(rgmRole);
+			List<Role> rgmRoleList = roleService.getListRole(rgmRole);
 			feedback_role.setRolename(Constants.ST_ROLE_NAME_FEEDBACKER);
-			try{
-				feedback_role = (Role) roleService.getListRole(feedback_role).get(0);
-			}catch (Exception e) {
-				throw new RuntimeException("角色["+Constants.ST_ROLE_NAME_FEEDBACKER+"]不存在");
+			try {
+				feedback_role = (Role) roleService.getListRole(feedback_role)
+						.get(0);
+			} catch (Exception e) {
+				throw new RuntimeException("角色["
+						+ Constants.ST_ROLE_NAME_FEEDBACKER + "]不存在");
 			}
-			
-			if(rgmRoleList!=null && rgmRoleList.size()>0){
-				rgmRole=rgmRoleList.get(0);
-				
-				User_role userrole=new User_role();
+
+			if (rgmRoleList != null && rgmRoleList.size() > 0) {
+				rgmRole = rgmRoleList.get(0);
+
+				User_role userrole = new User_role();
 				userrole.setRoleid(rgmRole.getRoleid());
-				List<User_role> lstUserRole = userRoleService.getListUser_role(userrole);
-				for(User_role userRole : lstUserRole){
-					//验证改用户是否具有反馈人角色，只有具有反馈人角色的用户才可以执行反馈。
-					userrole=new User_role();
+				List<User_role> lstUserRole = userRoleService
+						.getListUser_role(userrole);
+				for (User_role userRole : lstUserRole) {
+					// 验证改用户是否具有反馈人角色，只有具有反馈人角色的用户才可以执行反馈。
+					userrole = new User_role();
 					userrole.setRoleid(feedback_role.getRoleid());
 					userrole.setUserid(userRole.getUserid());
 					userrole = userRoleService.getUser_role(userrole);
-					if(userrole != null)
-						feedbackUsersStr+=","+userRole.getUserid();
+					if (userrole != null)
+						feedbackUsersStr += "," + userRole.getUserid();
 				}
-					
+
 			}
-			if(feedbackUsersStr.length()==0)
+			if (feedbackUsersStr.length() == 0)
 				throw new RuntimeException("所关联的地区区总，不具有反馈人角色");
-			feedbackUsersStr=feedbackUsersStr.substring(1);
+			feedbackUsersStr = feedbackUsersStr.substring(1);
 			candidateUsers.put("feedbackUsers", feedbackUsersStr);
-//			指派归档用户
-//			指派 技术质量管理员
-			Role role=new Role();
+			// 指派归档用户
+			// 指派 技术质量管理员
+			Role role = new Role();
 			role.setRolename(Constants.ST_ROLE_NAME_QC);
-			try{
-				role=(Role) roleService.getListRole(role).get(0);
-			}catch (IndexOutOfBoundsException e) {
-				throw new RuntimeException("角色["+Constants.ST_ROLE_NAME_QC+"] 未创建..");
+			try {
+				role = (Role) roleService.getListRole(role).get(0);
+			} catch (IndexOutOfBoundsException e) {
+				throw new RuntimeException("角色[" + Constants.ST_ROLE_NAME_QC
+						+ "] 未创建..");
 			}
-			
+
 			User_role userRole = new User_role();
-			userRole.setRoleid(role.getRoleid());//技术质量员角色
-			List<User_role> userRoleList = userRoleService.getListUser_role(userRole);
-			String archiverUserID="";
-			if(userRoleList!=null && userRoleList.size() > 0){
+			userRole.setRoleid(role.getRoleid());// 技术质量员角色
+			List<User_role> userRoleList = userRoleService
+					.getListUser_role(userRole);
+			String archiverUserID = "";
+			if (userRoleList != null && userRoleList.size() > 0) {
 				StringBuffer archiverUserIDBuffer = new StringBuffer();
-				for(User_role userRole1 : userRoleList){
+				for (User_role userRole1 : userRoleList) {
 					archiverUserIDBuffer.append(userRole1.getUserid());
 					archiverUserIDBuffer.append(",");
 				}
-				archiverUserID=archiverUserIDBuffer.substring(0, archiverUserIDBuffer.length()-1);
+				archiverUserID = archiverUserIDBuffer.substring(0,
+						archiverUserIDBuffer.length() - 1);
 			}
-			
+
 			candidateUsers.put("archiveUsers", archiverUserID);
-			
-//			添加总工审核相关的变量
+
+			// 添加总工审核相关的变量
 			params.put("ceApprovalCode", "");
-//			审核标志通过
+			// 审核标志通过
 			params.put("approvalCodePassed", Constants.ST_APPR_TYPE_APPR_PASSED);
-			//流程启动
-			ProcessInstance newpi = workflow.workflowStart(workflow.setVariable(null, null,
-					candidateUsers, params));
+			// 流程启动
+			ProcessInstance newpi = workflow.workflowStart(workflow
+					.setVariable(null, null, candidateUsers, params));
 			return newpi.getId();
-//			throw new RuntimeException("test");
-		}catch (RuntimeException e) {
+			// throw new RuntimeException("test");
+		} catch (RuntimeException e) {
 			log.error(e);
 			throw e;
-		} 
+		}
 
 	}
-	
-	@Resource(name="SupportTicketServiceImpl")
+
+	@Resource(name = "SupportTicketServiceImpl")
 	public void setStService(SupportTicketService stService) {
 		this.stService = stService;
 	}
 
-	@Resource(name="dict_itemService")
+	@Resource(name = "dict_itemService")
 	public void setDictitemService(IDict_itemService dictitemService) {
 		this.dictitemService = dictitemService;
 	}
-	
-	@Resource(name="user_roleService")
+
+	@Resource(name = "user_roleService")
 	public void setUserRoleService(IUser_roleService userRoleService) {
 		this.userRoleService = userRoleService;
 	}
 
-	@Resource(name="CommonUtil")
+	@Resource(name = "CommonUtil")
 	public void setUtil(CommonUtil util) {
 		this.util = util;
 	}
-	@Resource(name="roleService")
+
+	@Resource(name = "roleService")
 	public void setRoleService(IRoleService roleService) {
 		this.roleService = roleService;
 	}
-	@Resource(name="WorkflowUtil")
+
+	@Resource(name = "WorkflowUtil")
 	public void setWorkflow(WorkflowUtil workflow) {
 		this.workflow = workflow;
 	}
 
-	/* (non-Javadoc)
-	 * @see com.aisino2.techsupport.service.ApplyService#checkStNoIsExist(java.lang.String)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.aisino2.techsupport.service.ApplyService#checkStNoIsExist(java.lang
+	 * .String)
 	 */
 	public boolean checkStNoIsExist(String stNo) {
-		SupportTicket st=new SupportTicket();
+		SupportTicket st = new SupportTicket();
 		st.setStNo(stNo);
-		List<SupportTicket> lst=stService.getListSupportTicket(st);
-		if(lst.size()>0)
+		List<SupportTicket> lst = stService.getListSupportTicket(st);
+		if (lst.size() > 0)
 			return true;
 		else
 			return false;
 	}
-
 
 }
