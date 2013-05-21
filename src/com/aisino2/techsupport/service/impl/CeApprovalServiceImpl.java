677098d61f763205ed576eff6fd8113a91fe7edd
firefoxmmx2 <firefoxmmx@gmail.com>
2013-05-21 14:11:40 +0800

    修正公司重新审核后系统处理流程不正确

--- src/com/aisino2/techsupport/service/impl/CeApprovalServiceImpl.java
+++ src/com/aisino2/techsupport/service/impl/CeApprovalServiceImpl.java
@@ -11,6 +11,8 @@ import org.springframework.stereotype.Component;
 
 import com.aisino2.core.service.BaseService;
 import com.aisino2.techsupport.common.Constants;
+import com.aisino2.techsupport.dao.ISupportLeaderRelationDao;
+import com.aisino2.techsupport.domain.SupportLeaderRelation;
 import com.aisino2.techsupport.domain.SupportTicket;
 import com.aisino2.techsupport.domain.Tracking;
 import com.aisino2.techsupport.service.ICeApprovalService;
@@ -20,56 +22,69 @@ import com.aisino2.techsupport.workflow.WorkflowUtil;
 
 /**
  * 技术支持单 总工审核服务实现
- *
+ * 
  */
 @Component("CeApprovalServiceImpl")
 public class CeApprovalServiceImpl extends BaseService implements
 		ICeApprovalService {
 	private SupportTicketService stService;
 	private TrackingService trackingService;
-	
-//	流程服务
+	private ISupportLeaderRelationDao supportLeaderRelationDao;
+	// 流程服务
 	private WorkflowUtil workflow;
-	
-	private static Logger log=Logger.getLogger(CeApprovalServiceImpl.class);
-	
-	public void insertCeApproval(String taskId, SupportTicket st,Tracking tracking) {
-//		如果不通过，设置状态为公司审批未通过
-		if(tracking.getApprovalCode().equals(Constants.ST_APPR_TYPE_APPR_NOPASS))
+
+	private static Logger log = Logger.getLogger(CeApprovalServiceImpl.class);
+
+	@Resource(name = "supportLeaderDaoImpl")
+	public void setSupportLeaderRelationDao(
+			ISupportLeaderRelationDao supportLeaderRelationDao) {
+		this.supportLeaderRelationDao = supportLeaderRelationDao;
+	}
+
+	public void insertCeApproval(String taskId, SupportTicket st,
+			Tracking tracking) {
+		// 如果不通过，设置状态为公司审批未通过
+		if (tracking.getApprovalCode().equals(
+				Constants.ST_APPR_TYPE_APPR_NOPASS))
 			st.setStStatus(Constants.ST_STATUS_COMPANY_APPRAVAL_NOPASS);
 		else
 			st.setStStatus(Constants.ST_STATUS_WAIT_DEPARTMENT_APPRAVAL);
-		//更新最后操作时间
+		// 更新最后操作时间
 		st.setLastUpdateDate(new Date());
-//		保存支持单信息
+		// 保存支持单信息
 		stService.updateSupportTicket(st);
-//		保存审核意见信息
+		// 保存审核意见信息
 		trackingService.insertTracking(tracking);
-//		流程控制
-//		当提交的技术支持单，在本环节重新对单位做出了指派的时候，那么主要更新指派信息
-		Map<String, Object> candidate_users=new HashMap<String, Object>();
-//		取消对部门审批人的指派，指派改在具体的部门指派的监听器里面进行
-//		List<Department> deptlist= st.getSupportDeptList();
-//		candidate_users.putAll(workflow.setDeptAssigneeVariable(deptlist));
-		Map<String, Object> parammap=new HashMap<String, Object>();
+		// 兼容重新指派清除该支持单所有的负责人指派
+		SupportLeaderRelation supportLeaderRelation = new SupportLeaderRelation();
+		supportLeaderRelation.setStId(st.getId());
+		supportLeaderRelationDao.delete(supportLeaderRelation);
+		// 流程控制
+		// 当提交的技术支持单，在本环节重新对单位做出了指派的时候，那么主要更新指派信息
+		Map<String, Object> candidate_users = new HashMap<String, Object>();
+		// 取消对部门审批人的指派，指派改在具体的部门指派的监听器里面进行
+		// List<Department> deptlist= st.getSupportDeptList();
+		// candidate_users.putAll(workflow.setDeptAssigneeVariable(deptlist));
+		Map<String, Object> parammap = new HashMap<String, Object>();
 		parammap.put("ceApprovalCode", tracking.getApprovalCode());
-		//debug
-		
-//		流程提交
-		workflow.workflowNext(workflow.setVariable(taskId,null,candidate_users ,parammap));
+		// debug
+
+		// 流程提交
+		workflow.workflowNext(workflow.setVariable(taskId, null,
+				candidate_users, parammap));
 	}
-	
-	@Resource(name="SupportTicketServiceImpl")
+
+	@Resource(name = "SupportTicketServiceImpl")
 	public void setStService(SupportTicketService stService) {
 		this.stService = stService;
 	}
 
-	@Resource(name="TrackingServiceImpl")
+	@Resource(name = "TrackingServiceImpl")
 	public void setTrackingService(TrackingService trackingService) {
 		this.trackingService = trackingService;
 	}
 
-	@Resource(name="WorkflowUtil")
+	@Resource(name = "WorkflowUtil")
 	public void setWorkflow(WorkflowUtil workflow) {
 		this.workflow = workflow;
 	}
